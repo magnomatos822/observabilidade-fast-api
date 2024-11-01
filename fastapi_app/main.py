20b6a387a3a96af353b1a073f8f827955851c04b
Magno Matos <magnomatos822@gmail.com>
2024-11-01 08:00:29 -0300

    feat: Update Python version and improve logging levels in main.py

--- fastapi_app/main.py
+++ fastapi_app/main.py
@@ -10,12 +10,12 @@ from fastapi import FastAPI, Response
 from opentelemetry.propagate import inject
 from utils import PrometheusMiddleware, metrics, setting_otlp
 
-APP_NAME = os.environ.get("APP_NAME", "app")
-EXPOSE_PORT = os.environ.get("EXPOSE_PORT", 8000)
-OTLP_GRPC_ENDPOINT = os.environ.get("OTLP_GRPC_ENDPOINT", "http://10.10.120.79:4317")
+APP_NAME = os.environ.get("app")
+EXPOSE_PORT = os.environ.get(8000)
+OTLP_GRPC_ENDPOINT = os.environ.get("http://10.10.120.79:4317")
 
-TARGET_ONE_HOST = os.environ.get("TARGET_ONE_HOST", "app-b")
-TARGET_TWO_HOST = os.environ.get("TARGET_TWO_HOST", "app-c")
+TARGET_ONE_HOST = os.environ.get("app-b"s 
+TARGET_TWO_HOST = os.environ.get("app-c")
 
 app = FastAPI()
 
@@ -39,20 +39,20 @@ logging.getLogger("uvicorn.access").addFilter(EndpointFilter())
 
 @app.get("/")
 async def read_root():
-    logging.error("Hello World")
+    logging.info("Hello World")
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: Optional[str] = None):
-    logging.error("items")
+    logging.info("items")
     return {"item_id": item_id, "q": q}
 
 
 @app.get("/io_task")
 async def io_task():
     time.sleep(1)
-    logging.error("io task")
+    logging.info("io task")
     return "IO bound task finish!"
 
 
@@ -60,21 +60,21 @@ async def io_task():
 async def cpu_task():
     for i in range(1000):
         _ = i * i * i
-    logging.error("cpu task")
+    logging.info("cpu task")
     return "CPU bound task finish!"
 
 
 @app.get("/random_status")
 async def random_status(response: Response):
     response.status_code = random.choice([200, 200, 300, 400, 500])
-    logging.error("random status")
+    logging.info("random status")
     return {"path": "/random_status"}
 
 
 @app.get("/random_sleep")
 async def random_sleep(response: Response):
     time.sleep(random.randint(0, 5))
-    logging.error("random sleep")
+    logging.info("random sleep")
     return {"path": "/random_sleep"}
 
 
@@ -88,11 +88,11 @@ async def error_test(response: Response):
 async def chain(response: Response):
     headers = {}
     inject(headers)  # inject trace info to header
-    logging.critical(headers)
+    logging.info(headers)
 
     async with httpx.AsyncClient() as client:
         await client.get(
-            "http://localhost:8000/",
+            f"http://localhost:{EXPOSE_PORT}/",
             headers=headers,
         )
     async with httpx.AsyncClient() as client:
